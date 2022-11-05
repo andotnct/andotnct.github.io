d30c72c28416fcf8dece1648867d10c9185f700a
andotnct <mityugame@gmail.com>
2022-11-06 03:12:11 +0900

    Updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Reactだにょおおおんおんぉんぉんぉんおんんんんんんん（；ｗ；）
+          うわああんReactだにょおおおんおんぉんぉんぉんおんんんんんんん（；ｗ；）
         </p>
         <a
           className="App-link"
