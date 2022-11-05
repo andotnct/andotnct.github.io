c6d9dca72202906e32ee3c66c71315cedaa8f107
andotnct <mityugame@gmail.com>
2022-11-06 02:35:50 +0900

    change message test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Reactだにょおおおんおんぉんぉんぉんおんんんんんん！？！？！？
+          Reactだにょおおおんおんぉんぉんぉんおんんんんんん？？？？？
         </p>
         <a
           className="App-link"
